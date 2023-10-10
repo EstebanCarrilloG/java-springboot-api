a2cfd5fce37271a0bf4e99947c2989bb6be07677
Esteban Carrillo <estexav0996@gmail.com>
2023-10-10 18:38:19 -0500

    n/a

--- movies/src/main/java/com/edeptec/movies/MovieService.java
+++ movies/src/main/java/com/edeptec/movies/MovieService.java
@@ -1,6 +1,5 @@
 package com.edeptec.movies;
 
-import org.bson.types.ObjectId;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
@@ -15,7 +14,8 @@ public List<Movie> allMovies(){
     return movieRepository.findAll();
 
 }
-public Optional<Movie> singleMovie(ObjectId id){
-    return movieRepository.findById(id);
+public Optional<Movie> singleMovie(String imdbId){
+
+    return movieRepository.findMovieByImdbId(imdbId);
 }
 }
